b1a81e79ba4aa940115041bb72c5312404a7c35f
AdityaPanwar26 <105982753+AdityaPanwar26@users.noreply.github.com>
2025-04-21 18:14:27 +0530

    Add files via upload

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,32 @@
+const HeroSection =()=>{
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>Your Feet Deserve The Best</h1>
+                <p>Better than anyone</p>
+
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+                </div>
+
+            <div className="shopping">
+                <p>available on</p>
+            
+
+            <div className="brand-icon">
+                <img src="/Images/shop.png" alt="flipkart" />
+                
+            </div>
+        </div>
+        </div>
+
+            <div className="hero-image">
+                <img src="/Images/shoe_image.png" alt="shoes"/>
+            </div>
+        </main>
+
+    );
+};
+
+export default HeroSection;
